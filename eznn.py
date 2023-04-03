508974ae5afb03326d44f1bbcdd2ed0264e35d52
Isacco Faglioni <isaccofaglioni2000@gmail.com>
2023-04-03 15:19:10 +0200

    full implementation of the structure of a Model for RNN

--- eznn.py
+++ eznn.py
@@ -18,3 +18,29 @@ else:
     print("GPU not available, CPU used")
 
 
+class Model(nn.Module):
+    def __init__(self, input_size, output_size, hidden_dim, n_layers):
+        #the following line is for inheritance
+        super(Model, self).__init__()
+
+        self.hidden_dim = hidden_dim
+        self.n_layers = n_layers
+        ###single layer
+        self.rnn = nn.RNN(input_size, hidden_dim, n_layers, batch_first=True)   
+        self.fc = nn.Linear(hidden_dim, output_size)
+
+    def init_hidden(self, batch_size):
+    """ This method generates the first hidden state of zeros which we'll use in the forward pass"""
+    #to me this look a bit simplistic
+        hidden = torch.zeros(self.n_layers, batch_size, self.hidden_dim)
+        return hidden
+
+    def forward(self, x):
+        batch_size = x.size(0)
+        # Initializing hidden state for first input using method defined below
+        hidden = self.init_hidden(batch_size)
+        out, hidden = self.rnn(x, hidden)
+        out = out.contiguous().view(-1, self.hidden_dim)
+        out = self.fc(out)
+        return out, hidden
+    
